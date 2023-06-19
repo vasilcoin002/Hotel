14e236a82500e740b7074264a5f99c9a3ca6fefb
vasilcoin002 <vasilcoin002@gmail.com>
2023-06-20 01:59:36 +0300

    Fixes and features

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -19,60 +19,21 @@ window.addEventListener('scroll', () => {
 })
 
 //
-// Book a place page/ apartments-slider
+// Header / menu burger
 //
 
-const slider = document.querySelector('.slider')
-const slider__arrow_left = slider.querySelector('.slider__arrow_left')
-const slider__arrow_right = slider.querySelector('.slider__arrow_right')
-const slider_list = slider.querySelector('.slider__list')
-let position = 1
-const total_items = 5
-
-function slider_swap_right() {
-    slider_list.style.translate = -position * 33.3333 + "%"
-    position++
-}
-
-function slider_swap_left() {
-    slider_list.style.translate = (2-position) * 33.3333 + "%"
-    position--
-}
-
-function make_button_visible(button) {
-    if (button.classList.contains('displayed-none')) {
-        button.classList.remove('displayed-none');
-        setTimeout(() => {
-            button.classList.remove('opacity0')
-            button.classList.add('opacity07');}, 0)
-    }}
-
-function make_button_invisible(button) {
-    button.classList.remove('opacity07')
-    button.classList.add('opacity0');
-    setTimeout(() => {button.classList.add('displayed-none')}, 500);
-}
-
-slider__arrow_right.addEventListener('click', () => {
-    if (position < total_items - 3) {
-        slider_swap_right();
-        make_button_visible(slider__arrow_left);
-    } else if (position === total_items - 3){
-        slider_swap_right();
-        make_button_invisible(slider__arrow_right);
+const burger__button = document.querySelector('.header__burger-button')
+const burger = document.querySelector('.burger')
+burger__button.addEventListener('click', () => {
+    // burger__button.classList.toggle('burger_active')
+    // burger.classList.toggle('burger_active')
+    if (burger__button.classList.contains('burger_active')) {
+        burger__button.classList.remove('burger_active')
+        burger.classList.remove('burger_active')
+    } else {
+        burger__button.classList.add('burger_active')
+        burger.classList.add('burger_active')
     }
-});
+})
 
-slider__arrow_left.addEventListener('click', () => {
-    if (position > 2) {
-        slider_swap_left();
-        make_button_visible(slider__arrow_right);
-    } else if (position === 2){
-        slider_swap_left();
-        make_button_invisible(slider__arrow_left);
-    }
-});
-
-//
-//
-//
+console.log(window.location)
